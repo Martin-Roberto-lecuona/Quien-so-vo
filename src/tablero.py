dc65c4c26d694d51129ca9c5d411ac1dd0d2c97b
Fran <fgarcete@alumno.unlam.edu.ar>
2024-05-17 22:55:48 -0300

    port python
    presenta error al tratar crear el server, no se actualiza a la pantalla de tablero

---
+++ src/tablero.py
@@ -0,0 +1,51 @@
+import pyray as rl
+from personaje import Personaje
+
+FILAS = 3
+COLUMNAS = 5
+PADDING = 15
+
+class Tablero:
+    def __init__(self) -> None:
+        self._personajes = [[None] * COLUMNAS for _ in range(FILAS)]
+
+        ancho_celda = 150
+        alto_celda = 250
+        imagen_ruta = "/home/pilum/Facultad/programacion-concurrente/Quien-so-vo-py/img/Ricardo-Fort.png"
+
+        for i in range(FILAS):
+            for j in range(COLUMNAS):
+                pos_x = (j+1) * (ancho_celda+PADDING) - ancho_celda
+                pos_y = i*alto_celda
+
+                personaje = Personaje(
+                    pos_x,
+                    pos_y,
+                    ancho_celda,
+                    alto_celda,
+                    imagen_ruta
+                )
+
+                self._personajes[i][j] = Personaje(
+                    pos_x,
+                    pos_y,
+                    ancho_celda,
+                    alto_celda,
+                    imagen_ruta
+                )
+
+    def dibujar(self):
+        for i in range(FILAS):
+            for j in range(COLUMNAS):
+                self._personajes[i][j].dibujar()
+
+    def on_click(self, mouse_pos):
+        for i in range(FILAS):
+            for j in range(COLUMNAS):
+                if (self._personajes[i][j].on_click(mouse_pos)):
+                    self._personajes[i][j].ajustar_opacidad()
+
+
+                
+            
+                
